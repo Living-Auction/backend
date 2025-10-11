05556527d7bbfaf0cb585d1c7c42df38fe0b2bfd
dongheonchoi <124031425+dongheonchoi@users.noreply.github.com>
2025-10-11 10:25:02 +0900

    test: activate 'test' profile for application context loading

--- src/test/java/com/project/livingauction/LivingAuctionApplicationTests.java
+++ src/test/java/com/project/livingauction/LivingAuctionApplicationTests.java
@@ -3,6 +3,7 @@ package com.project.livingauction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class LivingAuctionApplicationTests {
 
