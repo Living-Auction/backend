358625211853124903e0dfe5863e1b464be51c56
Dongheon <ehdgjs855@gmail.com>
2025-10-13 10:25:57 +0900

    test: swagger cors test

---
+++ src/main/java/com/project/livingauction/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.project.livingauction.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000",
+                        "http://living-auction.com/",
+                        "https://living-auction.com/",
+                        "https://living-auction.com:5173",
+                        "https://kfe3-wonderful-app.vercel.app" )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+    }
+}
